38f9e748db62b50c77109518745712ff8f39c5be
M D <4567776+mdimec4@users.noreply.github.com>
2020-09-11 00:18:15 +0200

    Create .ycm_extra_conf.py

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""
+Python source code - replace this with a description of the code and write the code below this text.
+"""
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+
+def Settings( **kwargs ):
+    return {
+        'interpreter_path': '/home/miha/.local/share/virtualenvs/joan-t3JbNkur/bin/python'
+    }
